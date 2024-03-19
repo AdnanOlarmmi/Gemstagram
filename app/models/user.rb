2c4a9320f8725003c9e1627124f762bc5747dd93
adnanolarmmi <olarmmi@gmail.com>
2024-03-19 14:52:43 +0100

    Add Follow Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,28 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+
   has_many :posts
   has_one_attached :profile_picture
   has_many :likes
   has_many :comments
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+
+  has_many :follow_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "followed_id"
+
+  has_many :accepted_received_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: "followed_id"
+
+  has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: "follower_id"
+
+
+  has_many :follower, through :accepted_received_requests, source: :follower
+  has_many :followings, through :accepted_sent_requests, source: :followed
+  # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
+  # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
+  # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
+
 
 end
