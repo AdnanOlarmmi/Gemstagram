cef0c686889b1411706a45094d1a7cabb7a8984b
adnanolarmmi <olarmmi@gmail.com>
2024-03-20 01:51:33 +0100

    Complete Follow Modal and Logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,19 @@ class User < ApplicationRecord
   has_many :accepted_sent_requests, -> {where(accepted: true)}, class_name: "Follow", foreign_key: "follower_id"
 
 
-  has_many :follower, through :accepted_received_requests, source: :follower
-  has_many :followings, through :accepted_sent_requests, source: :followed
+  has_many :followers, through: :accepted_received_requests, source: :follower
+  has_many :followings, through: :accepted_sent_requests, source: :followed
+
+  def follow(user)
+    unless user == self
+      Follow.create(follower: self, followed: user)
+    end
+  end
+
+  def unfollow(user)
+    self.accepted_sent_requests.find_by(followed: user)&.destroy
+    # Follow.destroy(follower: self, followed: user)
+  end
   # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
   # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
