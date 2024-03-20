520aa26cf911f15ee9ecc47661051236bf4e4be3
adnanolarmmi <olarmmi@gmail.com>
2024-03-20 02:06:22 +0100

    Add Follows route and Controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ApplicationRecord
   has_many :followers, through: :accepted_received_requests, source: :follower
   has_many :followings, through: :accepted_sent_requests, source: :followed
 
+  has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
+
   def follow(user)
     unless user == self
       Follow.create(follower: self, followed: user)
@@ -31,9 +33,13 @@ class User < ApplicationRecord
     self.accepted_sent_requests.find_by(followed: user)&.destroy
     # Follow.destroy(follower: self, followed: user)
   end
+
+  def cancel_request(user)
+    self.waiting_sent_requests.find_by(followed: user)&.destroy
+  end
   # has_many :received_requests, class_name: "Follow", foreign_key: "followed_id"
   # has_many :sent_requests, class_name: "Follow", foreign_key: "follower_id"
-  # has_many :waiting_sent_requests, -> {where(accepted: false)}, class_name: "Follow", foreign_key: "follower_id"
+
 
 
 end
