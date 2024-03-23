9977cfcc14b03f7ee2d504fb191f8f486c66fd5b
adnanolarmmi <olarmmi@gmail.com>
2024-03-23 15:49:55 +0100

    Complete Search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-    before_action :set_user
+    before_action :set_user, only: [:show]
 
-    def show
-        
+    def show    
+    end
+
+    def index
+        if params[:search_query].present?
+            @users = User.where("LOWER(username) LIKE ?", "%#{params[:search_query].downcase}%")
+        else
+            @users = []
+        end
+
+        if turbo_frame_request?
+            render partial: "layouts/search_results", locals: {users: @users} 
+        end
     end
 
     private
