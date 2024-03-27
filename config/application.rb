3f8fc37edb07e93d8710a31ba991bc5cedc90d75
adnanolarmmi <olarmmi@gmail.com>
2024-03-27 16:45:41 +0100

    Fix

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Gemstagram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.0 # Choosing Rails 6 defaults
+
+    config.autoloader = :zeitwerk
 
     # Configuration for the application, engines, and railties goes here.
     #
