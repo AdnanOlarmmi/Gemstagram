b08ae6c5dd1884e0501a282dcd300184a01dadd1
adnanolarmmi <olarmmi@gmail.com>
2024-03-27 12:15:18 +0100

    Fixes

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-
+config.autoload_lib(ignore: %w[assets tasks generators])
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
