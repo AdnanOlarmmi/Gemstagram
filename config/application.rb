b744e992fad109810a19e07c30cab83ff2cab6aa
adnanolarmmi <olarmmi@gmail.com>
2024-03-27 15:47:23 +0100

    Fixes

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
 require_relative "boot"
 
 require "rails/all"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Gemstagram
   class Application < Rails::Application
-    config.autoload_paths(ignore: %w[assets tasks generators])
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
